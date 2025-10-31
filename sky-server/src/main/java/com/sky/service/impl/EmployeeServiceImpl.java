88bb00be36f1c7bd46758177b0f9e799056b0cdf
ZhenTong <910898041@qq.com>
2025-10-31 10:48:02 +0800

    Day02-01-code-improvement
    Refactor employee creation to use dynamic user ID and add SQL exception handling

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -84,8 +85,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //设置当前记录人创建id和修改人id（暂时写死为10）
         // TODO : 后续改为动态获取当前登录用户的id
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
 
         //保存员工信息到数据库
         employeeMapper.insert(employee);
