37b2ab4466db55e44b8d7cbeaf6acc25997d1f9f
ZhenTong <910898041@qq.com>
2025-11-29 16:56:41 +0800

    Day09-04-add-admin-order-management-functionality

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -42,4 +42,13 @@ public interface OrderMapper {
      */
     @Select("select * from orders where id = #{id}")
     Orders getById(Long id);
+
+
+    /**
+     * 根据状态统计订单数量
+     * @param status 订单状态
+     * @return 订单数量
+     */
+    @Select("select count(id) from orders where status = #{status}")
+    Integer countStatus(Integer status);
 }
